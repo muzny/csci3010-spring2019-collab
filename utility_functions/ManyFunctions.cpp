927399e2f4bc53b2f6af269da0695396d12dd5f0
Mohamed Abdalla <moabdalla246@gmail.com>
2019-02-05 12:15:56 -0700

    added Split() function

--- utility_functions/ManyFunctions.cpp
+++ utility_functions/ManyFunctions.cpp
@@ -1,1 +1,19 @@
 #include "ManyFunctions.h"
+// By: Tyler Benson, Mohamed Abdalla
+
+class ManyFunctions {
+	
+	vector<string> ManyFunctions::Split(string whole, string seperator){
+		string s = whole;
+		string delimiter = seperator;
+
+		size_t pos = 0;
+		std::string token;
+		while ((pos = s.find(delimiter)) != std::string::npos) {
+			token = s.substr(0, pos);
+			std::cout << token << std::endl;
+			s.erase(0, pos + delimiter.length());
+		}
+		words.push_back(s);
+	}
+}
