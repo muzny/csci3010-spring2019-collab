d5830f6d3fcfc5417f987f7ea020ad839a570adb
Elizabeth <31430045+qlychee@users.noreply.github.com>
2019-02-05 12:16:49 -0700

    add test case

--- utility_functions/main.cpp
+++ utility_functions/main.cpp
@@ -2,4 +2,7 @@
 
 int main() {
     // make sure to run your functions!
+    std::string s1 = "hi";
+    std::string s2 = "hihelloworld";
+    std::cout<<RemoveFirstSubstring(std::string s1, std::string s2);
 }
