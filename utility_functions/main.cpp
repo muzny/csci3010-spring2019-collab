503a836ca7027add81379380f6ef40f449d981c3
zzihan1206 <zizh1583@colorado.edu>
2019-02-07 11:33:53 -0700

    111

--- utility_functions/main.cpp
+++ utility_functions/main.cpp
@@ -9,4 +9,11 @@ int main() {
     {
     	std::cout<<b[i]<<std::endl;
     }
+
+    std::vector<int> bit_1 = {8888,76757,4567,88890,67532};
+    std::vector<bool> b_1 = EvenMask(bit);
+    for (int i = 0; i < b_1.size(); ++i)
+    {
+    	std::cout<<b_1[i]<<std::endl;
+    }
 }
