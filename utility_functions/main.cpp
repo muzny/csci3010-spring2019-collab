7f97c6e026bee1347bb1f23b940bf1d9ffb65a99
hainsworth99 <31012794+hainsworth99@users.noreply.github.com>
2019-02-07 11:42:13 -0700

    added main test

--- utility_functions/main.cpp
+++ utility_functions/main.cpp
@@ -1,5 +1,13 @@
 #include "ManyFunctions.h"
+#include <vector>
+#include <iostream>
 
 int main() {
-    // make sure to run your functions!
+	std::vector<int> testVectorTimesN = {1,2,3};
+	testVectorTimesN = VectorTimesN(testVectorTimesN,4);
+	for (int i = 0; i < testVectorTimesN.size(); i++){
+		std::cout << testVectorTimesN[i] << " ";
+	}
+	std::cout << std::endl;
+	return 0;
 }
