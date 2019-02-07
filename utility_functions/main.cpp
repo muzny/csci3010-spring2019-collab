61fd040f90acd82423e265550ed11f89c82b4d39
Adam <adam.tenhoeve@colorado.edu>
2019-02-07 12:12:37 -0700

    Updated main.cpp to include more test cases to include edge cases for negatives and 0.

--- utility_functions/main.cpp
+++ utility_functions/main.cpp
@@ -4,10 +4,26 @@
 
 int main() {
     // make sure to run your functions!
-	std::vector<int> nums;
-	nums.push_back(3);
-	nums.push_back(7);
-	nums.push_back(5);
+    // Beginning of tests for Adam, Ash and Thomas' Product function
+	// Initial test case for product.
+	std::vector<int> test1;
+	test1.push_back(3);
+	test1.push_back(7);
+	test1.push_back(5);
+	// Test case for negative numbers
+	std::vector<int> test2;
+	test2.push_back(-1);
+	test2.push_back(5);
+	// Test case for 0.
+	std::vector<int> test3;
+	test3.push_back(4);
+	test3.push_back(0);
 
-	std::cout << Product(nums) << std::endl;
+	// Should return 105
+	std::cout << Product(test1) << std::endl;
+	// Should return -5
+	std::cout << Product(test2) << std::endl;
+	// Should return 0
+	std::cout << Product(test3) << std::endl;
+	// End of tests for Adam, Ash and Thomas' Product Function
 }
