2bd2408ec3383d2ef3e2c3b11017323a7cda6ca2
jackson boynton <jacksonboynton@cu-engr2-1-12-10.201.46.126.int.colorado.edu>
2019-02-07 11:44:07 -0700

    Weve uploaded

--- utility_functions/ManyFunctions.cpp
+++ utility_functions/ManyFunctions.cpp
@@ -1,1 +1,15 @@
 #include "ManyFunctions.h"
+
+
+std::string RemoveAllSubstrings(std::string s1, std::string s2) {
+
+	size_t pos = std::string::npos;
+
+	if(s1.length() < s2.length()) return s1;
+	if(s1 == s2) return "";
+	while((pos = s1.find(s2)) != std::string::npos) {
+		s1.erase(pos, s2.length());
+	}
+
+	return s1;
+}
--- utility_functions/main.cpp
+++ utility_functions/main.cpp
@@ -1,5 +1,13 @@
 #include "ManyFunctions.h"
+#include <iostream>
 
 int main() {
     // make sure to run your functions!
+
+    std::cout << RemoveAllSubstrings("Banana", "nana") << std::endl;
+    std::cout<<RemoveAllSubstrings("This is a this is a string", "is")<<std::endl;
+    std::cout<<RemoveAllSubstrings("", "")<<std::endl;
+    std::cout<<RemoveAllSubstrings("blah", "very blah")<<std::endl;
+
+    std::cout<<"Program terminated successfully"<<std::endl;
 }
