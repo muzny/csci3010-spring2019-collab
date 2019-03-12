328bdc761c45ee5114c2a7358cd4760d8ea06432
sabrinatouchCU <sabrina.touch@colorado.edu>
2019-03-11 20:38:58 -0600

    Fixed issues in code

--- utility_functions/ManyFunctions.cpp
+++ utility_functions/ManyFunctions.cpp
@@ -1,11 +1,11 @@
 #include "ManyFunctions.h"
 int Sum(int num1, int num2){
-	return num1+num2;
+	return num1 + num2;
 }
 
 std::vector<int> VectorPlusN(std::vector<int>v, int n){
-	for(int i=0; i<v.size(); i++){
-		v[i]=v[i]+n;
+	for(int i = 0; i < v.size(); i++){
+		v[i] = v[i] + n;
 	}
 
 	return v;
--- utility_functions/main.cpp
+++ utility_functions/main.cpp
@@ -1,10 +1,28 @@
 #include "ManyFunctions.h"
+#include <iostream>
 
 int main() {
     // make sure to run your functions!
-    Sum(7,10);
+    // Sum()
+    int x = 7;
+    int y = 10;
+    std::cout << x << " + " << y << " = " << Sum(x, y) << std::endl;
+
+    // VectorPlusN()
+    std::vector<int> v{1, 2, 3, 4};
+
+    std::cout << "v before call to VectorPlusN: ";
+    for(int i : v) {
+      std::cout << i << " ";
+    }
+    std::cout << "\n" << std::endl;
+
+    std::vector<int> returned_vector = VectorPlusN(v, 5);
+
+    std::cout << "v after call to VectorPlusN: ";
+    for(int i : returned_vector) {
+      std::cout << i << " ";
+    }
+    std::cout << "\n" << std::endl;
+
 }
-
-std::vector<int> v{1,2,3,4};
-
-std::vector<int> VectorPlusN(v, 5);
